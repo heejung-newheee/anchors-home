4c06d3c3a991c329bdfea8727e2ca45044280ebc
chchaa <chchaa@ancrhos-biz.com>
2023-04-07 18:41:59 +0900

    react next setting , guide base

--- app/guide/page.js
+++ app/guide/page.js
@@ -1,9 +1,6 @@
 import './guide.scss';
-import localFont from "next/font/local";
-const covered = localFont({ src: [{path : '../../public/assets/font/coveredByYourGrace.woff',weight : '400',},]})
-const pretendard = localFont({ src: [{path : '../../public/assets/font/Pretendard-Regular.woff', weight : '400',}, {path : '../../public/assets/font/Pretendard-Medium.woff', weight : '500',}, {path : '../../public/assets/font/Pretendard-Bold.woff', weight : '700',},]})
-const stolzl = localFont({src: [{path : '../../public/assets/font/stolzl-Regular.woff', weight : '400',}, {path : '../../public/assets/font/Stolzl-Medium.woff', weight : '500',}, {path : '../../public/assets/font/Stolzl-Bold.woff', weight : '700',},]})
-
+import {getFontDefinitions} from "@/helper/init-helper";
+const {covered, pretendard, stolzl} = getFontDefinitions();
 
 export default function Guide() {
     return (
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Header from "@/components/Header/Header";
+// import Header from "@/components/Header/Header";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <>
-     <Header />
+     {/*<Header />*/}
      <Link href="guide">guide</Link><br/>
      <Link href="pages/about">about</Link>
    </>
