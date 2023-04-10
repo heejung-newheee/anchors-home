f5afee37015b59421272a871c59408018140c981
chchaa <chchaa@ancrhos-biz.com>
2023-04-10 14:56:05 +0900

    common css 파일명 정리

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
-import '@/public/assets/css/_reset.scss';
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
+import '@/public/assets/css/common.scss';
 // import {getFontDefinitions} from "@/helper/init-helper";
 // const {covered, pretendard, stolzl} = getFontDefinitions();
 
