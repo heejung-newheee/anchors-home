69359917ce2384f66f59177d1409ca103462c661
chchaa <chchaa@ancrhos-biz.com>
2023-04-10 14:20:44 +0900

    common component shape

---
+++ components/AccordionTitle/AccordionTitle.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import Link from "next/link";
+
+export default function AccordionTitle() {
+  // props list = activeOption
+
+  return (
+      <dt className="accordion-title"><!--타이틀--></dt>
+  )
+}
