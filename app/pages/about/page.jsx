50d1eaa9f1471752b8d1f79ba81717baf226d65f
JUEUN <leejueun0091@gmail.com>
2023-04-12 18:01:58 +0900

    ElementTitle 컴포넌트 마크업 / Visual 컴포넌트 마크업, scss

--- app/pages/about/page.jsx
+++ app/pages/about/page.jsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import './about.scss'
+import ElementTitle from '@/components/ElementTitle/ElementTitle'
+import Visual from '@/components/Visual/Visual'
+import SwiperArea from '@/components/SwiperArea/SwiperArea'
 
 export const metadata = {
   title: 'Anchors',
@@ -15,6 +18,20 @@ export default function About() {
     <div>
       {/*<img src="/assets/images/ico/ico-btn-black-down.svg"/>*/}
       <p>this is about</p>
+
+      {/* ElementTitle component test */}
+      <ElementTitle data="Element Title"/>
+
+      {/* Visual component test */}
+      <Visual imgUrl="https://picsum.photos/1920/800" dimm="Y">
+        <div>
+          <p>Title</p>
+          <span>Description</span>
+        </div>
+      </Visual>
+
+      {/* SwiperArea component test */}
+      <SwiperArea />
     </div>
   )
 }
