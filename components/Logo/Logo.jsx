e1d7ebf2d38a4e06d111ba48be9e8f7e61bd9be2
chchaa <chchaa@ancrhos-biz.com>
2023-04-12 11:24:33 +0900

    json 디렉토리 구조 변경

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -2,16 +2,17 @@
 import Image from 'next/image'
 import Link from "next/link";
 import Btn from "@/components/Btn/Btn";
-import {headerFooter} from "@/public/assets/json/header_footer";
+import headerFooter from "@/helper/data/json/headerFooter.json";
 
 
 export default function Logo() {
-  console.log(headerFooter)
 
   return (
-      <h1 className="logo">{/*header_footer.js 호출*/}
+      <h1 className="logo">{/*headerFooter.json 호출*/}
         <Btn type="link" url="/" alt="Anchors">
         {/*  로고 이미지  */}
+          {JSON.stringify(headerFooter)}
+          {headerFooter.copyright}
         </Btn>
       </h1>
   )
