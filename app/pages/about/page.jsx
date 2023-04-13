4f38fbb76a36c97d34f61968a0329b4f32b46570
mijeongcho <mjcho@anchors-biz.com>
2023-04-13 19:40:09 +0900

    [2023-04-12] lottie 컴포넌트 작업

--- app/pages/about/page.jsx
+++ app/pages/about/page.jsx
@@ -1,12 +1,23 @@
-"use client"
-import Image from 'next/image'
-// import './about.scss'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import './about.scss';
+import LottiePlayer from '@/components/Lottie/LottiePlayer';
 
-export default function About() {
+export const metadata = {
+  title: 'Anchors',
+  description: '',
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+function About() {
   return (
     <div>
       {/*<img src="/assets/images/ico/ico-btn-black-down.svg"/>*/}
-      <p>this is about</p>
+      <p >this is about!!!!</p>
+      <LottiePlayer render="svg" style={{height : '600px', width : '600px', "backgroundColor" : "#1D1D1F"}} src={"/lottie/lottie_aboutus.json"}/>
     </div>
   )
 }
+
+export  default  About;
