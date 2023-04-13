0cd3e483aa3d103d418e36fc7baa69d062a3834a
JUEUN <leejueun0091@gmail.com>
2023-04-13 21:36:55 +0900

    SwiperArea, Visual component 수정

--- app/guide/test/jueun/page.jsx
+++ app/guide/test/jueun/page.jsx
@@ -1,23 +1,49 @@
-"use client"
-import {initHelper} from "@/helper/init-helper";
-import ElementTitle from '@/components/ElementTitle/ElementTitle'
-import Visual from '@/components/Visual/Visual'
-const {jquery , variables} = initHelper();
+'use client';
+import React from 'react';
 
-export default function jueun() {
+import ElementTitle from '@/components/ElementTitle/ElementTitle';
+import SwiperArea from '@/components/SwiperArea/SwiperArea';
+import Visual from '@/components/Visual/Visual';
+import jquery from '@/helper/libs/jquery_loader';
+import variables from '@/public/assets/scss/_variables.module.scss';
+
+function swiperContent() {
+  return <div>single swiper</div>;
+}
+
+function firstContent() {
+  return <div>first swiper</div>;
+}
+
+function secondContent() {
+  return <div>double swiper</div>;
+}
+
+export default function Jueun() {
   return (
-      <>
-        {/* ElementTitle component test */}
-        <ElementTitle data="Element Title"/>
+    <>
+      {/* ElementTitle component test */}
+      <p style={{ backgroundColor: variables.colorBlack, color: '#fff', padding: '10px 0' }}>ElementTitle Component</p>
+      <ElementTitle data="Element Title" />
 
-        {/* Visual component test */}
-        <Visual imgUrl="https://picsum.photos/1920/800">
-          <div>
-            <p>Title</p>
-            <span>Description</span>
-          </div>
-        </Visual>
+      {/* Visual component test */}
+      <p style={{ backgroundColor: variables.colorBlack, color: '#fff', padding: '10px 0', marginTop: '30px' }}>Visual Component</p>
+      <Visual imgUrl="https://picsum.photos/1920/800">
+        <div>
+          <p>Title</p>정<span>Description</span>
+        </div>
+      </Visual>
 
-      </>
-  )
+      {/* SwiperArea component test */}
+      <p style={{ backgroundColor: '#000', color: '#fff', padding: '10px 0', marginTop: '30px' }}>SwiperArea Component</p>
+      <SwiperArea
+        type="single"
+        swiperContent={swiperContent}
+        swiperOption={{
+          slidesPerView: 1,
+        }}
+      />
+      <SwiperArea type="double" firstContent={firstContent} secondContent={secondContent} />
+    </>
+  );
 }
--- components/SwiperArea/SwiperArea.jsx
+++ components/SwiperArea/SwiperArea.jsx
@@ -1,13 +1,63 @@
-"use client"
+'use client';
+import React from 'react';
 
-import './scss/SwiperArea.scss'
+import './scss/SwiperArea.scss';
 
-export default function SwiperArea() {
+// S: swiper import
+import { Pagination, Navigation, Scrollbar, EffectFade, EffectCube, EffectCoverflow, EffectFlip, EffectCards, EffectCreative, Controller } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import 'swiper/css/effect-fade';
+import 'swiper/css/effect-cube';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/effect-flip';
+import 'swiper/css/effect-cards';
+import 'swiper/css/effect-creative';
+// E: swiper import
 
+const swiperModule = [Pagination, Navigation, Scrollbar, EffectFade, EffectCube, EffectCoverflow, EffectFlip, EffectCards, EffectCreative, Controller];
 
-  return (
+const DEFAULT_SWIPER_OPTION = {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  effect: 'slide',
+  pagination: { clickable: true },
+  navigation: true,
+  scrollbar: false,
+};
+
+function SwiperArea({ type, swiperOption = DEFAULT_SWIPER_OPTION, swiperContent, firstContent, secondContent }) {
+  const [firstSwiper, setFirstSwiper] = React.useState(null);
+  const [secondSwiper, setSecondSwiper] = React.useState(null);
+
+  const computedSwiperOption = { ...DEFAULT_SWIPER_OPTION, ...swiperOption };
+
+  if (type === 'double') {
+    return (
       <>
+        <Swiper modules={swiperModule} onSwiper={setFirstSwiper} controller={{ control: secondSwiper }}>
+          <SwiperSlide>{firstContent}</SwiperSlide>
+          <SwiperSlide>{firstContent}</SwiperSlide>
+          <SwiperSlide>{firstContent}</SwiperSlide>
+        </Swiper>
+        <Swiper modules={swiperModule} onSwiper={setSecondSwiper} controller={{ control: firstSwiper }}>
+          <SwiperSlide>{secondContent}</SwiperSlide>
+          <SwiperSlide>{secondContent}</SwiperSlide>
+          <SwiperSlide>{secondContent}</SwiperSlide>
+        </Swiper>
       </>
-  )
-
+    );
+  }
+  return (
+    <Swiper modules={swiperModule} {...computedSwiperOption}>
+      <SwiperSlide>{swiperContent}</SwiperSlide>
+      <SwiperSlide>{swiperContent}</SwiperSlide>
+      <SwiperSlide>{swiperContent}</SwiperSlide>
+    </Swiper>
+  );
 }
+
+export default SwiperArea;
