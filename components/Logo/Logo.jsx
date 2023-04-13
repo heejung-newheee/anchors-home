3662f063af6ca9c5d9f90c34804062bdc2200e67
chchaa <chchaa@ancrhos-biz.com>
2023-04-13 21:35:22 +0900

    accordion component

--- components/Logo/Logo.jsx
+++ components/Logo/Logo.jsx
@@ -1,4 +1,3 @@
-"use client"
 import Image from 'next/image'
 import Link from "next/link";
 import Btn from "@/components/Btn/Btn";
@@ -7,19 +6,19 @@ import headerFooter from "@/helper/data/json/contents/headerFooter.json";
 export default function Logo() {
 
   return (
-      <h1 className="logo">{/*headerFooter.json 호출*/}
+      <h1 className="logo">
         <Btn type="link" url="/" alt="Anchors">
-        {/*  로고 이미지  */}
-          {JSON.stringify(headerFooter)}
-          {headerFooter.copyright}
+        {/*  {JSON.stringify(headerFooter)}*/}
+        {/*  {headerFooter.copyright}*/}
+        {/*  <Image*/}
+        {/*    src={headerFooter.sendEmail}*/}
+        {/*    width={100}*/}
+        {/*    height={100}*/}
+        {/*    className="image-width"*/}
+        {/*    alt="Anchors"*/}
+        {/*  />*/}
+          logo
         </Btn>
-        <Image
-        src={headerFooter.sendEmail}
-        width={100}
-        height={100}
-        className="image-width"
-        alt="Anchors"
-        />
       </h1>
   )
 }
