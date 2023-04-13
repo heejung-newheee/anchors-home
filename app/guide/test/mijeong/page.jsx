c5c552c3af7b06d688139448ec82482ef1179ebb
mijeongcho <mjcho@anchors-biz.com>
2023-04-13 20:25:57 +0900

    [2023-04-13] lottie 컴포넌트 작업

--- app/guide/test/mijeong/page.jsx
+++ app/guide/test/mijeong/page.jsx
@@ -1,9 +1,18 @@
-"use client"
-import {initHelper} from "@/helper/init-helper";
-const {jquery , variables} = initHelper();
+'use client';
+import LottiePlayer from '@/components/Lottie/LottiePlayer';
+import { initHelper } from '@/helper/init-helper';
+const { jquery, variables } = initHelper();
 
-export default function mijeong() {
+function mijeong() {
   return (
-  <></>
-  )
+    <>
+      <p> this is chomj!!!! </p>
+      <LottiePlayer
+        render="svg"
+        style={{ height: '600px', width: '600px', backgroundColor: '#1D1D1F' }}
+        src={'/lottie/lottie_aboutus.json'}
+      />
+    </>
+  );
 }
+export default mijeong;
--- components/Lottie/LottiePlayer.jsx
+++ components/Lottie/LottiePlayer.jsx
@@ -1,32 +1,42 @@
-"use client";
+'use client';
 
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 /**
- * 로티 플레이어
- * @param loop 반복합니다.
- * @param autoplay 자동재생
- * @param render 랜더방법 : "svg","canvas"
+ * LOTTIE OPTION
+ * @param loop
+ * @param autoplay
+ * @param render
  * @param src
  * @param style
+ * @param visible
+ * @param buttons
  * @returns {JSX.Element}
  * @constructor
  */
-function LottiePlayer({loop =true, autoplay=true, render="svn", src, style={}, visible=false}, buttons=['play', 'repeat', 'frame', 'debug']) {
+
+function LottiePlayer({
+  loop = true,
+  autoplay = true,
+  render = 'svn',
+  src,
+  style = {},
+  visible = false,
+  buttons = ['play', 'repeat', 'frame', 'debug'],
+}) {
+  const computedStyle = { ...{}, ...style };
+
   return (
     <Player
       loop={loop}
       autoplay={autoplay}
       renderer={render}
-      style={style}
+      style={computedStyle}
       src={src}
     >
-      <Controls
-        visible={visible}
-        buttons={buttons}
-      />
+      <Controls visible={visible} buttons={buttons} />
     </Player>
-  )
+  );
 }
 
 export default LottiePlayer;
