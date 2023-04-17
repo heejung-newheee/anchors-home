656705fae6185ef81e51d35e33d87bf5f1054267
chchaa <chchaa@ancrhos-biz.com>
2023-04-17 10:50:50 +0900

    test page 생성

--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,28 @@
-import "./scss/main.scss";
-import Image from 'next/image'
-import Link from "next/link";
+import './scss/main.scss';
+import Image from 'next/image';
+import Link from 'next/link';
 // import Header from "@/components/Header/Header";
 
 export const metadata = {
   title: 'Anchors',
   description: 'Anchors',
-}
+};
 
 export default function Main() {
   return (
-   <>
-     {/*<Header />*/}
-     <Link href="guide" className="name-is-cha" style={{lineHeight: "10px"}}>guide</Link><br/>
-     <Link href="pages/about">about</Link> <br/> <br/>
-
-     <p className="text">mediaQuery type - </p> <br/> <br/>
-
-     <Link href="guide/test/chchaa">chchaa test page</Link> <br/>
-     <Link href="guide/test/jueun">jueun test page</Link> <br/>
-     <Link href="guide/test/mijeong">mijeong test page</Link>
-
-   </>
-  )
+    <>
+      {/*<Header />*/}
+      <Link href="guide" className="name-is-cha" style={{ lineHeight: '10px' }}>
+        guide
+      </Link>
+      <br />
+      <Link href="pages/about">about</Link> <br /> <br />
+      <p className="text">mediaQuery type - </p> <br /> <br />
+      <Link href="guide/test/chchaa">chchaa test page</Link> <br />
+      <Link href="guide/test/jueun">jueun test page</Link> <br />
+      <Link href="guide/test/mijeong">mijeong test page</Link> <br />
+      <Link href="guide/test/doi">doi test page</Link> <br />
+      <Link href="guide/test/chaewon">chaewon test page</Link>
+    </>
+  );
 }
