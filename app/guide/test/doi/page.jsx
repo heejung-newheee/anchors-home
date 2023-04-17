2fe8457eca5bebb5436453028ff09d64b44550f6
“doih” <doiviih@anchors-biz.com>
2023-04-17 10:41:06 +0900

    [2023-04-17] Video 컴포넌트 작업

--- app/guide/test/doi/page.jsx
+++ app/guide/test/doi/page.jsx
@@ -1,15 +1,14 @@
-"use client";
-
+'use client';
 
 import Disclaimer from '@/components/Disclaimer/Disclaimer';
 import Video from '@/components/Video/Video';
-
+import Image from 'next/image';
 
 export default function Doi() {
   return (
-      <>
-        <Disclaimer data="disclaimer" />
-        <Video videoUrl="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />
-      </>
-  )
+    <>
+      <Disclaimer data="disclaimer" />
+      <Video videoUrl="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />
+    </>
+  );
 }
