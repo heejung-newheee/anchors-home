1b1556ac84014aec3f07e3ddbd97bec3e25b357b
“doih” <doiviih@anchors-biz.com>
2023-04-17 16:34:19 +0900

    [2023-04-17] Video 컴포넌트 작업

--- components/Video/Video.jsx
+++ components/Video/Video.jsx
@@ -3,33 +3,32 @@
 import Image from 'next/image';
 // import Link from "next/link";
 import videoPlayBtn from '@/public/assets/images/ico/ico-btn-play.svg';
+import Btn from '@/components/Btn/Btn';
 import './scss/Video.scss';
-import { useState } from 'react';
 
 export default function Video({ videoUrl }) {
-  const [videoBtnState, setVideoBtnState] = useState('block');
   const videoPlay = () => {
     document.querySelector('video').play();
     document.querySelector('video').classList.add('on');
-    setVideoBtnState('none');
+    document.querySelector('button').classList.add('hide');
   };
   return (
     <div className="video-wrap">
       <video src={videoUrl} controls></video>
 
-      <button
-        className="video-btn"
+      <Btn
+        design="video-btn"
         type="button"
-        style={{ display: videoBtnState }}
         onClick={videoPlay}
-      >
-        <Image
-          src={videoPlayBtn}
-          width={60}
-          height={60}
-          alt="video play button"
-        />
-      </button>
+        children={
+          <Image
+            src={videoPlayBtn}
+            width={60}
+            height={60}
+            alt="video play button"
+          />
+        }
+      />
     </div>
   );
 }
