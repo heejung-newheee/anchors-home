fb57401528212fb0c854beccc3e6e362dfc764c0
chchaa <chchaa@ancrhos-biz.com>
2023-04-17 10:51:55 +0900

    test page 생성

--- app/guide/test/chaewon/page.jsx
+++ app/guide/test/chaewon/page.jsx
@@ -1,14 +1,7 @@
 'use client';
 
-import Disclaimer from '@/components/Disclaimer/Disclaimer';
-import Video from '@/components/Video/Video';
 import Image from 'next/image';
 
-export default function Doi() {
-  return (
-    <>
-      <Disclaimer data="disclaimer" />
-      <Video videoUrl="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />
-    </>
-  );
+export default function Chaewon() {
+  return <></>;
 }
