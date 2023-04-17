5df826090cca93d37680332a7a2112033015915f
“doih” <doiviih@anchors-biz.com>
2023-04-17 16:43:52 +0900

    [2023-04-17] Video 컴포넌트 작업

--- components/Video/Video.jsx
+++ components/Video/Video.jsx
@@ -6,18 +6,18 @@ import videoPlayBtn from '@/public/assets/images/ico/ico-btn-play.svg';
 import Btn from '@/components/Btn/Btn';
 import './scss/Video.scss';
 
-export default function Video({ videoUrl }) {
+export default function Video({ videoUrl, className }) {
   const videoPlay = () => {
     document.querySelector('video').play();
     document.querySelector('video').classList.add('on');
     document.querySelector('button').classList.add('hide');
   };
   return (
-    <div className="video-wrap">
+    <div className={`video ${className}`}>
       <video src={videoUrl} controls></video>
 
       <Btn
-        design="video-btn"
+        className="video-btn"
         type="button"
         onClick={videoPlay}
         children={
