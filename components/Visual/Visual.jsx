e883d273f9a0a9dda49193c92edb7e7d8bab6909
JUEUN <leejueun0091@gmail.com>
2023-04-18 11:02:00 +0900

    Visual component scss 작업 / Menu component scss 수정

--- components/Visual/Visual.jsx
+++ components/Visual/Visual.jsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import './Visual.scss';
+import './scss/Visual.scss';
 
 export default function Visual({ imgUrl, dimm = 'N', children, className }) {
   const dimmClass = dimm === 'Y' ? ' dimmed' : '';
