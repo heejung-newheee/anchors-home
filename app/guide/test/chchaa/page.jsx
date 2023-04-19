883ef720d9ca46ccaf76b0f25b53e2e70c7e719d
JUEUN <leejueun0091@gmail.com>
2023-04-19 16:55:40 +0900

    함수명, 변수명 수정

--- app/guide/test/chchaa/page.jsx
+++ app/guide/test/chchaa/page.jsx
@@ -34,7 +34,7 @@ const PAGETITLE_DATA = [{ word: 'Our' }, { word: 'Works' }];
 export default function Chchaa() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <PageTitle data={headerFooter.menuList[0].name} />
       <div
         className="section-div is-white"
