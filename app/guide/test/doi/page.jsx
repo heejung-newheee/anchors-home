c628fbbfdd744ce98807d8efd213e9723a681900
“doih” <doiviih@anchors-biz.com>
2023-04-19 18:38:24 +0900

    [2023-04-19] doi test 페이지 커밋

--- app/guide/test/doi/page.jsx
+++ app/guide/test/doi/page.jsx
@@ -1,15 +1,21 @@
 'use client';
-
-import Image from 'next/image';
+import React from 'react';
 
 import Disclaimer from '@/components/Disclaimer/Disclaimer';
 import Video from '@/components/Video/Video';
+import Footer from '@/components/Footer/Footer';
+import Image from 'next/image';
+import RootLayout from '@/app/layout';
+import WorksTemplate from '@/components/WorksTemplate/WorksTemplate';
+import { content } from '@/helper/data/json/contents/works/works.json';
 
 export default function Doi() {
   return (
     <>
-      <Disclaimer data="disclaimer" />
-      <Video videoUrl="http://commondatastorage.googleapis.com/gtv_videos_bucket/sample/ElephantsDream.mp4" />
+      {/* <Disclaimer data="disclaimer" /> */}
+      <Video videoUrl="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" />
+      <Footer />
+      <WorksTemplate />
     </>
   );
 }
