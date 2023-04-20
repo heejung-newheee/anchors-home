d45eb6d5dddd26ed289951ea67d2e4fb05c3c320
chchaa <chchaa@ancrhos-biz.com>
2023-04-20 13:05:04 +0900

    guide component 영역 삭제

--- app/guide/page.jsx
+++ app/guide/page.jsx
@@ -25,9 +25,6 @@ export default function Guide() {
           <li>
             <Link href="guide/pages/button">Button</Link>
           </li>
-          <li>
-            <Link href="guide/pages/component">Component</Link>
-          </li>
         </ul>
       </header>
       <main>
