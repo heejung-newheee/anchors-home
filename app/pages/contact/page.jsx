732bbe495401da70f07cdd58db77f1a1b17d49f9
JUEUN <leejueun0091@gmail.com>
2023-04-21 18:05:36 +0900

    contact 페이지 작업 / visual component scss 수정 / contact.json 수정

--- app/pages/contact/page.jsx
+++ app/pages/contact/page.jsx
@@ -1,3 +1,11 @@
+import BaseArticle from '@/components/BaseArticle/BaseArticle';
+import Description from '@/components/Description/Description';
+import PageTitle from '@/components/PageTitle/PageTitle';
+import Visual from '@/components/Visual/Visual';
+import contact from '@/helper/data/json/contents/contact/contact.json';
+import headerFooter from '@/helper/data/json/contents/headerFooter.json';
+import './scss/contact.scss';
+
 export const metadata = {
   title: 'Anchors',
   description: '',
@@ -5,10 +13,27 @@ export const metadata = {
 
 function Contact() {
   return (
-    <div>
-      {/*<img src="/assets/images/ico/ico_btn_download_black.svg"/>*/}
-      <p>this is contact!!!!</p>
-    </div>
+    <main className="contact">
+      <PageTitle data={headerFooter.menuList[3].name} />
+      <Visual imgUrl={contact.imgUrl + contact.img}>
+        <span className="wordbubble">
+          <img
+            src={contact.imgUrl + contact.bubbleImg}
+            alt={contact.bubbleAlt}
+          />
+        </span>
+        <article className="contact_cont_wrap">
+          <div className="contact_address">
+            <Description data={contact.address} />
+          </div>
+          <div className="contact_info">
+            <BaseArticle elementTitle="Email" description={contact.email} />
+            <BaseArticle elementTitle="Phone" description={contact.phone} />
+            <BaseArticle elementTitle="Fax" description={contact.fax} />
+          </div>
+        </article>
+      </Visual>
+    </main>
   );
 }
 
