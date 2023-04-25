58dad53f43e076acfecff97b3db58622d904defa
JUEUN <leejueun0091@gmail.com>
2023-04-25 16:19:20 +0900

    service 페이지 작업 / service.json 수정

--- app/pages/service/page.jsx
+++ app/pages/service/page.jsx
@@ -1,3 +1,12 @@
+import './scss/service.scss';
+import Description from '@/components/Description/Description';
+import Disclaimer from '@/components/Disclaimer/Disclaimer';
+import ElementTitle from '@/components/ElementTitle/ElementTitle';
+import PageTitle from '@/components/PageTitle/PageTitle';
+import Visual from '@/components/Visual/Visual';
+import headerFooter from '@/helper/data/json/contents/headerFooter.json';
+import service from '@/helper/data/json/contents/service/service.json';
+
 export const metadata = {
   title: 'Anchors',
   description: '',
@@ -5,10 +14,62 @@ export const metadata = {
 
 function Service() {
   return (
-    <div>
-      {/*<img src="/assets/images/ico/ico_btn_download_black.svg"/>*/}
-      <p>this is service!!!!</p>
-    </div>
+    <main className="service">
+      <PageTitle data={headerFooter.menuList[2].name} />
+      <section className="visual_comment">
+        <Visual imgUrl="/assets/images/contents/service/img_keyvisual_officewide.png" />
+        <Description
+          className="en_comment"
+          data={
+            <>
+              Anchors is a <span>software development</span> company established
+              in 2018. Although short in history, we are Powered by
+              <span>Technology</span> and Driven by <span>Talent.</span>
+            </>
+          }
+        />
+        <Description
+          className="ko_comment"
+          data="앵커스는 2018년에 설립된 소프트웨어 개발 회사입니다.
+회사의 짧은 역사지만, 우리는 기술에 의해 구동되고 인재에 의해 구동됩니다."
+        />
+      </section>
+      <section className="anchors_part">
+        {service.introduction.map((cont, idx) => (
+          <>
+            <div key={idx} className="container">
+              <span className="part_img">
+                <img src={service.imgUrl + cont.img} alt={cont.alt} />
+              </span>
+              <article className="part_introduction_wrap">
+                <ElementTitle data={cont.name} />
+                <div className="description_wrap">
+                  <Description
+                    data={
+                      <>
+                        <span>{cont.textLine1}</span>
+                        <span>{cont.textLine2}</span>
+                      </>
+                    }
+                  />
+                  <Disclaimer
+                    data={
+                      <>
+                        {cont.hashtag.map((tag, idx) => (
+                          <span key={idx} className="hashtag">
+                            {tag}
+                          </span>
+                        ))}
+                      </>
+                    }
+                  />
+                </div>
+              </article>
+            </div>
+          </>
+        ))}
+      </section>
+    </main>
   );
 }
 
