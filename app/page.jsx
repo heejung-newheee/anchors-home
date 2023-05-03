8a8bb463a0259838ed88e3979dbbbbacf7f6807b
chchaa <chchaa@ancrhos-biz.com>
2023-05-03 15:22:41 +0900

    main scrolltrigger 적용

--- app/page.jsx
+++ app/page.jsx
@@ -31,33 +31,6 @@ function Main() {
             />
           </div>
           {/*RotateRolling*/}
-          {/*<SwiperArea*/}
-          {/*  type="single"*/}
-          {/*  swiperOption={{*/}
-          {/*    */}
-          {/*    */}
-          {/*    slidesPerView: 3,*/}
-          {/*    allowTouchMove: false,*/}
-          {/*    navigation: false,*/}
-          {/*    pagination: false,*/}
-          {/*    autoplay: {*/}
-          {/*      delay: 1,*/}
-          {/*    },*/}
-          {/*    loop: true,*/}
-          {/*    speed: 1000,*/}
-          {/*  }}*/}
-          {/*  className="scroll-linear"*/}
-          {/*  swiperContent={*/}
-          {/*    <>*/}
-          {/*      <div>*/}
-          {/*        <img*/}
-          {/*          src="/assets/images/contents/main/img_expertise03.png"*/}
-          {/*          alt=""*/}
-          {/*        />*/}
-          {/*      </div>*/}
-          {/*    </>*/}
-          {/*  }*/}
-          {/*/>*/}
           <div className="intro_area">
             <Description
               className="intro_en"
@@ -84,47 +57,159 @@ function Main() {
         {/* S: service */}
         <section className="service">
           <div className="section_div is_black bg_blue rolling_wrap">
-            <span className="rolling_text">
-              <ScrollTriggerArea
-                type="multiTrigger"
-                triggerStart={{ mobile: '100vh' }}
-                triggerEnd={{ mobile: '5000vh' }}
-                triggerOffset={'center'}
-                triggerMarkers={false}
-                XAxes={{
-                  mobile: ['-400vh', '-400vh'],
-                }}
-                duration={0.01}
-                // toScale={}
-              >
-                {/*{main.service.rollingBigText.map((data, index) => (*/}
-                {/*  <span key={index}>{data}</span>*/}
-                {/*))}*/}
+            <ScrollTriggerArea
+              type="multiTrigger"
+              triggerStart={{
+                mobile: '1200vh',
+                tablet: '0px',
+                desktop: '0px',
+                wide: '0px',
+              }}
+              triggerEnd={{
+                mobile: '2200vh',
+                tablet: '0px',
+                desktop: '0px',
+                wide: '0px',
+              }}
+              triggerOffset={'center'}
+              triggerMarkers={false}
+              XAxes={{
+                mobile: ['-360vh'],
+                tablet: ['-400vh'],
+                desktop: ['-400vh'],
+                wide: ['-400vh'],
+              }}
+              duration={0.001}
+            >
+              <span className="rolling_text">
                 <span>
                   {main.service.rollingBigText[0]}
                   <ScrollTriggerArea
                     type="multiTrigger"
-                    triggerStart={{ mobile: '100vh' }}
-                    triggerEnd={{ mobile: '5000vh' }}
+                    triggerStart={{
+                      mobile: '1150vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
+                    triggerEnd={{
+                      mobile: '1650vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
                     triggerOffset={'center'}
                     triggerMarkers={false}
                     XAxes={{
-                      mobile: ['-150vh', '-150vh', '-150vh'],
+                      mobile: ['-200%'],
+                      tablet: ['-200%'],
+                      desktop: ['-200%'],
+                      wide: ['-200%'],
                     }}
                     duration={0.01}
                   >
-                    {main.service.blueBgRollingImages.map((data, index) => (
-                      <img
-                        key={index}
-                        src={main.imgUrl + data.image}
-                        alt={data.alt}
-                      />
-                    ))}
+                    <img
+                      src={
+                        main.imgUrl + main.service.blueBgRollingImages[0].image
+                      }
+                      alt={main.service.blueBgRollingImages[0].alt}
+                    />
+                  </ScrollTriggerArea>
+
+                  <ScrollTriggerArea
+                    type="multiTrigger"
+                    triggerStart={{
+                      mobile: '800vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
+                    triggerEnd={{
+                      mobile: '1300vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
+                    triggerOffset={'center'}
+                    triggerMarkers={false}
+                    XAxes={{
+                      mobile: ['-200%'],
+                      tablet: ['-200%'],
+                      desktop: ['-200%'],
+                      wide: ['-200%'],
+                    }}
+                    duration={0.01}
+                  >
+                    <img
+                      src={
+                        main.imgUrl + main.service.blueBgRollingImages[1].image
+                      }
+                      alt={main.service.blueBgRollingImages[1].alt}
+                    />
+                  </ScrollTriggerArea>
+
+                  <ScrollTriggerArea
+                    type="multiTrigger"
+                    triggerStart={{
+                      mobile: '1200vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
+                    triggerEnd={{
+                      mobile: '1700vh',
+                      tablet: '0px',
+                      desktop: '0px',
+                      wide: '0px',
+                    }}
+                    triggerOffset={'center'}
+                    triggerMarkers={false}
+                    XAxes={{
+                      mobile: ['-200%'],
+                      tablet: ['-200%'],
+                      desktop: ['-200%'],
+                      wide: ['-200%'],
+                    }}
+                    duration={0.01}
+                  >
+                    <img
+                      src={
+                        main.imgUrl + main.service.blueBgRollingImages[2].image
+                      }
+                      alt={main.service.blueBgRollingImages[2].alt}
+                    />
                   </ScrollTriggerArea>
                 </span>
-                <span>{main.service.rollingBigText[1]}</span>
-              </ScrollTriggerArea>
-            </span>
+
+                <ScrollTriggerArea
+                  type="multiTrigger"
+                  triggerStart={{
+                    mobile: '1650vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerEnd={{
+                    mobile: '2150vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerOffset={'center'}
+                  triggerMarkers={false}
+                  toScale={[30]}
+                  YAxes={{
+                    mobile: ['-200%'],
+                    tablet: ['-200%'],
+                    desktop: ['-200%'],
+                    wide: ['-200%'],
+                  }}
+                  duration={0.001}
+                >
+                  <span>{main.service.rollingBigText[1]}</span>
+                </ScrollTriggerArea>
+              </span>
+            </ScrollTriggerArea>
           </div>
           <div className="section_div is_white bg_white">
             <div className="swiper_bundle">
@@ -233,61 +318,170 @@ function Main() {
         {/* S: our works */}
         <section className="our_works">
           <div className="section_div is_white rolling_wrap">
-            {/*<ScrollTriggerArea*/}
-            {/*  type="multiTrigger"*/}
-            {/*  triggerStart={{*/}
-            {/*    mobile: '50vh',*/}
-            {/*    table: '0px',*/}
-            {/*    desktop: '50px',*/}
-            {/*    wide: '50px',*/}
-            {/*  }}*/}
-            {/*  triggerEnd={{*/}
-            {/*    mobile: '10000vh',*/}
-            {/*    table: '0px',*/}
-            {/*    desktop: '600px',*/}
-            {/*    wide: '600px',*/}
-            {/*  }}*/}
-            {/*  triggerOffset={'center'}*/}
-            {/*  triggerMarkers={true}*/}
-            {/*  XAxes={{*/}
-            {/*    mobile: ['-600vw'],*/}
-            {/*    table: ['0px'],*/}
-            {/*    desktop: ['500px'],*/}
-            {/*    wide: ['700px'],*/}
-            {/*  }}*/}
-            {/*  duration={20}*/}
-            {/*>*/}
-            <span className="rolling_text">{main.ourWorks.rollingBigText}</span>
-            <div className="rolling_imgs">
-              {main.ourWorks.rollingImages.map((data, index) => (
-                <img
-                  key={index}
-                  src={main.imgUrl + data.image}
-                  alt={data.alt}
-                />
-              ))}
-            </div>
-            {/*</ScrollTriggerArea>*/}
+            <ScrollTriggerArea
+              type="multiTrigger"
+              triggerStart={{
+                mobile: '3500vh',
+                tablet: '0px',
+                desktop: '0px',
+                wide: '0px',
+              }}
+              triggerEnd={{
+                mobile: '4500vh',
+                tablet: '0px',
+                desktop: '0px',
+                wide: '0px',
+              }}
+              triggerOffset={'center'}
+              triggerMarkers={false}
+              XAxes={{
+                mobile: ['-360vh'],
+                tablet: ['-400vh'],
+                desktop: ['-400vh'],
+                wide: ['-400vh'],
+              }}
+              duration={0.001}
+            >
+              <span className="rolling_text">
+                {main.ourWorks.rollingBigText}
+                <ScrollTriggerArea
+                  type="multiTrigger"
+                  triggerStart={{
+                    mobile: '3500vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerEnd={{
+                    mobile: '4000vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerOffset={'center'}
+                  triggerMarkers={false}
+                  XAxes={{
+                    mobile: ['-200%'],
+                    tablet: ['-200%'],
+                    desktop: ['-200%'],
+                    wide: ['-200%'],
+                  }}
+                  duration={0.01}
+                >
+                  <img
+                    src={main.imgUrl + main.ourWorks.rollingImages[0].image}
+                    alt={main.ourWorks.rollingImages[0].alt}
+                  />
+                </ScrollTriggerArea>
+
+                <ScrollTriggerArea
+                  type="multiTrigger"
+                  triggerStart={{
+                    mobile: '3400vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerEnd={{
+                    mobile: '3900vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerOffset={'center'}
+                  triggerMarkers={false}
+                  XAxes={{
+                    mobile: ['-200%'],
+                    tablet: ['-200%'],
+                    desktop: ['-200%'],
+                    wide: ['-200%'],
+                  }}
+                  duration={0.01}
+                >
+                  <img
+                    src={main.imgUrl + main.ourWorks.rollingImages[1].image}
+                    alt={main.ourWorks.rollingImages[1].alt}
+                  />
+                </ScrollTriggerArea>
+
+                <ScrollTriggerArea
+                  type="multiTrigger"
+                  triggerStart={{
+                    mobile: '3500vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerEnd={{
+                    mobile: '4000vh',
+                    tablet: '0px',
+                    desktop: '0px',
+                    wide: '0px',
+                  }}
+                  triggerOffset={'center'}
+                  triggerMarkers={false}
+                  XAxes={{
+                    mobile: ['-200%'],
+                    tablet: ['-200%'],
+                    desktop: ['-200%'],
+                    wide: ['-200%'],
+                  }}
+                  duration={0.01}
+                >
+                  <img
+                    src={main.imgUrl + main.ourWorks.rollingImages[2].image}
+                    alt={main.ourWorks.rollingImages[2].alt}
+                  />
+                </ScrollTriggerArea>
+              </span>
+            </ScrollTriggerArea>
           </div>
-          <SwiperArea
-            type="single"
-            swiperOption={{
-              slidesPerView: 'auto',
-              effect: 'cards',
-              cardsEffect: {
-                perSlideRotate: 30,
-                perSlideOffset: 100,
-              },
+          <ScrollTriggerArea
+            type="multiTrigger"
+            triggerStart={{
+              mobile: '4000vh',
+              tablet: '0px',
+              desktop: '0px',
+              wide: '0px',
             }}
-            className="section_div is_photo scroll-linear portfolio"
-            swiperContent={
-              <>
-                {main.ourWorks.portfolio.map((data, index) => (
-                  <FinderWorksList key={index} arrLength={index} />
-                ))}
-              </>
-            }
-          />
+            triggerEnd={{
+              mobile: '4500vh',
+              tablet: '0px',
+              desktop: '0px',
+              wide: '0px',
+            }}
+            triggerOffset={'center'}
+            triggerMarkers={false}
+            XAxes={{
+              mobile: ['-100%'],
+              tablet: ['-100%'],
+              desktop: ['-100%'],
+              wide: ['-100%'],
+            }}
+            duration={0.01}
+          >
+            <div className="portfolio_wrap">
+              <SwiperArea
+                type="single"
+                swiperOption={{
+                  slidesPerView: 'auto',
+                  effect: 'cards',
+                  cardsEffect: {
+                    perSlideRotate: 30,
+                    perSlideOffset: 100,
+                  },
+                }}
+                className="section_div is_photo scroll-linear portfolio"
+                swiperContent={
+                  <>
+                    {main.ourWorks.portfolio.map((data, index) => (
+                      <FinderWorksList key={index} arrLength={index} />
+                    ))}
+                  </>
+                }
+              />
+            </div>
+          </ScrollTriggerArea>
         </section>
         {/* E: our works */}
       </main>
