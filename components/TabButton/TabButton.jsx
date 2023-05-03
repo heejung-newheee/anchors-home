b0cef97d1d8e9775dfb715d4bdf34b270d62fffa
mijeongcho <mjcho@anchors-biz.com>
2023-05-03 17:58:27 +0900

    [2023-05-03] works 작업진행 중

--- components/TabButton/TabButton.jsx
+++ components/TabButton/TabButton.jsx
@@ -1,16 +1,20 @@
 'use client';
 import Btn from '@/components/Btn/Btn';
 
-export default function TabButton({ dataList = [] }) {
+function TabButton({ dataList = [], onClick, className }) {
   const BUTTON_ARR = Array.isArray(dataList) ? dataList : [dataList];
 
   return (
     <ul className="tab_btn_wrap">
       {BUTTON_ARR.map((button, idx) => (
         <li className="tab-button" data-tab-index={idx} key={idx}>
-          <Btn type="button">{button}</Btn>
+          <Btn type="button" onClick={onClick} className={className}>
+            {button}
+          </Btn>
         </li>
       ))}
     </ul>
   );
 }
+
+export default TabButton;
