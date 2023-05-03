5ab305c159fcf9b9d74251481aa1d482c8d31492
chchaa <chchaa@ancrhos-biz.com>
2023-05-03 19:10:33 +0900

    main auto rolling

--- app/guide/test/chchaa/page.jsx
+++ app/guide/test/chchaa/page.jsx
@@ -12,150 +12,152 @@ import ImageArray from '@/components/ImageArray/ImageArray';
 import Logo from '@/components/Logo/Logo';
 import PageTitle from '@/components/PageTitle/PageTitle';
 import Visual from '@/components/Visual/Visual';
+import WebGlCarousel from '@/components/WebGlCarousel/WebGlCarousel';
 import headerFooter from '@/helper/data/json/contents/headerFooter.json';
 import variables from '@/public/assets/scss/_variables.module.scss';
 import './chchaa.scss';
 
-const ACCORDION_CONTENTS = [
-  {
-    title: '1',
-    content: (
-      <>
-        fff
-        <br />
-        asdfasdf
-      </>
-    ),
-  },
-  { title: '2', content: <span>asdf</span> },
-  { title: '3', content: <div>name</div> },
-];
+// const ACCORDION_CONTENTS = [
+//   {
+//     title: '1',
+//     content: (
+//       <>
+//         fff
+//         <br />
+//         asdfasdf
+//       </>
+//     ),
+//   },
+//   { title: '2', content: <span>asdf</span> },
+//   { title: '3', content: <div>name</div> },
+// ];
 //headerFooter.menuList[0].name
 
-const PAGETITLE_DATA = [{ word: 'Our' }, { word: 'Works' }];
-
-const DESCRIPTION_DATA = 'description<b>tag text</b>';
-
-const IMGARR_DATA = [{ url: '/assets/images/이미지', alt: 'alt값' }];
+// const PAGETITLE_DATA = [{ word: 'Our' }, { word: 'Works' }];
+//
+// const DESCRIPTION_DATA = 'description<b>tag text</b>';
+//
+// const IMGARR_DATA = [{ url: '/assets/images/이미지', alt: 'alt값' }];
 export default function Chchaa() {
   return (
     <>
-      <ImageArray data={IMGARR_DATA} bgColor="#ff0000" />
-      <Description data={DESCRIPTION_DATA} innerHTMLOption="Y" />
-      <Description data={DESCRIPTION_DATA} innerHTMLOption="N" />
-      <Description data={DESCRIPTION_DATA} />
-      {/*<Accordion contents={headerFooter.test} />*/}
-      {/*<Description dangerouslySetInnerHTML={{ __html: headerFooter.test }} />*/}
-      <Header className="new_header is_black" />
-      <div className="is_black" />
-      <div
-        className="section_div is_black"
-        style={{
-          backgroundColor: variables.colorBlue,
-          color: variables.colorWhite,
-          height: '200px',
-        }}
-      >
-        어두운 배경
-      </div>
-      <PageTitle data={headerFooter.menuList[0].name} />
-      <div
-        className="section_div is_white"
-        style={{
-          backgroundColor: variables.colorLightgrey,
-          color: variables.colorWhite,
-          height: '200px',
-        }}
-      >
-        밝은 배경
-      </div>
-      <div
-        className="section_div is_black"
-        style={{
-          backgroundColor: variables.colorBlack,
-          color: variables.colorWhite,
-          height: '200px',
-        }}
-      >
-        어두운 배경
-      </div>
-      <div
-        className="section02"
-        style={{
-          backgroundColor: variables.colorWhite,
-          color: variables.colorWhite,
-          height: '200px',
-        }}
-      >
-        void
-      </div>
-      <div
-        className="section_div is_photo"
-        style={{
-          backgroundColor: variables.colorLightblue,
-          color: variables.colorWhite,
-          height: '200px',
-        }}
-      >
-        사진 배경
-      </div>
-      1. &lt;Btn&gt;
-      <br />
-      <Btn design="btnSecondaryUp">테스트 버튼</Btn>
-      <br />
-      <br />
-      2. &lt;Logo&gt;
-      <br />
-      <Logo />
-      <br />
-      <br />
-      2. &lt;Accordion&gt; <br />
-      <p style={{ backgroundColor: variables.colorBlue }}>
-        data props(contents) [&#123;title : "data", content : "data"&#125; , ...
-        ]
-      </p>
-      <br />
-      <Accordion
-        className="accordion1"
-        contents={[
-          {
-            title: '1',
-            content: (
-              <>
-                fff123123123123
-                <br />
-                asdfasdf
-              </>
-            ),
-          },
-          { title: '2', content: <span>asdf</span> },
-          { title: '3', content: <div>name</div> },
-        ]}
-      />
-      <br />
-      <br />
-      3. &lt;BaseArticle&gt;
-      <br />
-      <BaseArticle className="bascArticle1" elementTitle="text" />
-      <br />
-      <br />
-      4. &lt;PageTitle&gt; <br />
-      <p>data props(data) [&#123;word : "data"&#125;, ...]</p>
-      <PageTitle data={PAGETITLE_DATA} />
-      <br />
-      <br />
-      5. &lt;DepthTitle&gt; <br />
-      <p />
+      <WebGlCarousel />
+      {/*<ImageArray data={IMGARR_DATA} bgColor="#ff0000" />*/}
+      {/*<Description data={DESCRIPTION_DATA} innerHTMLOption="Y" />*/}
+      {/*<Description data={DESCRIPTION_DATA} innerHTMLOption="N" />*/}
+      {/*<Description data={DESCRIPTION_DATA} />*/}
+      {/*/!*<Accordion contents={headerFooter.test} />*!/*/}
+      {/*/!*<Description dangerouslySetInnerHTML={{ __html: headerFooter.test }} />*!/*/}
+      {/*<Header className="new_header is_black" />*/}
+      {/*<div className="is_black" />*/}
+      {/*<div*/}
+      {/*  className="section_div is_black"*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: variables.colorBlue,*/}
+      {/*    color: variables.colorWhite,*/}
+      {/*    height: '200px',*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  어두운 배경*/}
+      {/*</div>*/}
+      {/*<PageTitle data={headerFooter.menuList[0].name} />*/}
+      {/*<div*/}
+      {/*  className="section_div is_white"*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: variables.colorLightgrey,*/}
+      {/*    color: variables.colorWhite,*/}
+      {/*    height: '200px',*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  밝은 배경*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className="section_div is_black"*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: variables.colorBlack,*/}
+      {/*    color: variables.colorWhite,*/}
+      {/*    height: '200px',*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  어두운 배경*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className="section02"*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: variables.colorWhite,*/}
+      {/*    color: variables.colorWhite,*/}
+      {/*    height: '200px',*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  void*/}
+      {/*</div>*/}
+      {/*<div*/}
+      {/*  className="section_div is_photo"*/}
+      {/*  style={{*/}
+      {/*    backgroundColor: variables.colorLightblue,*/}
+      {/*    color: variables.colorWhite,*/}
+      {/*    height: '200px',*/}
+      {/*  }}*/}
+      {/*>*/}
+      {/*  사진 배경*/}
+      {/*</div>*/}
+      {/*1. &lt;Btn&gt;*/}
+      {/*<br />*/}
+      {/*<Btn design="btnSecondaryUp">테스트 버튼</Btn>*/}
+      {/*<br />*/}
+      {/*<br />*/}
+      {/*2. &lt;Logo&gt;*/}
+      {/*<br />*/}
+      {/*<Logo />*/}
+      {/*<br />*/}
+      {/*<br />*/}
+      {/*2. &lt;Accordion&gt; <br />*/}
+      {/*<p style={{ backgroundColor: variables.colorBlue }}>*/}
+      {/*  data props(contents) [&#123;title : "data", content : "data"&#125; , ...*/}
+      {/*  ]*/}
+      {/*</p>*/}
+      {/*<br />*/}
+      {/*<Accordion*/}
+      {/*  className="accordion1"*/}
+      {/*  contents={[*/}
+      {/*    {*/}
+      {/*      title: '1',*/}
+      {/*      content: (*/}
+      {/*        <>*/}
+      {/*          fff123123123123*/}
+      {/*          <br />*/}
+      {/*          asdfasdf*/}
+      {/*        </>*/}
+      {/*      ),*/}
+      {/*    },*/}
+      {/*    { title: '2', content: <span>asdf</span> },*/}
+      {/*    { title: '3', content: <div>name</div> },*/}
+      {/*  ]}*/}
+      {/*/>*/}
+      {/*<br />*/}
+      {/*<br />*/}
+      {/*3. &lt;BaseArticle&gt;*/}
+      {/*<br />*/}
+      {/*<BaseArticle className="bascArticle1" elementTitle="text" />*/}
+      {/*<br />*/}
+      {/*<br />*/}
+      {/*4. &lt;PageTitle&gt; <br />*/}
+      {/*<p>data props(data) [&#123;word : "data"&#125;, ...]</p>*/}
       {/*<PageTitle data={PAGETITLE_DATA} />*/}
-      <DepthTitle blindOption="" depthLevel="2">
-        depthTitle
-      </DepthTitle>
-      6. disclaimer <br />
-      <Disclaimer className="disclaimer1 disclaimer2" data="disclaimer" />
-      <br />
-      6. visual <br />
-      <Visual className="visual1" />
-      <BaseArticle disclaimer="disclaimer" description="description" />
+      {/*<br />*/}
+      {/*<br />*/}
+      {/*5. &lt;DepthTitle&gt; <br />*/}
+      {/*<p />*/}
+      {/*/!*<PageTitle data={PAGETITLE_DATA} />*!/*/}
+      {/*<DepthTitle blindOption="" depthLevel="2">*/}
+      {/*  depthTitle*/}
+      {/*</DepthTitle>*/}
+      {/*6. disclaimer <br />*/}
+      {/*<Disclaimer className="disclaimer1 disclaimer2" data="disclaimer" />*/}
+      {/*<br />*/}
+      {/*6. visual <br />*/}
+      {/*<Visual className="visual1" />*/}
+      {/*<BaseArticle disclaimer="disclaimer" description="description" />*/}
     </>
   );
 }
--- components/WebGlCarousel/WebGlCarousel.js
+++ components/WebGlCarousel/WebGlCarousel.js
@@ -7,50 +7,26 @@ import Carousel from './Carousel';
 import './scss/WebGlCarousel.scss';
 
 const images = [
-  {
-    image: 'assets/images/contents/main/carousel/1.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/2.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/3.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/4.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/5.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/6.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/7.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/8.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/9.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/10.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/11.jpg',
-  },
-  {
-    image: 'assets/images/contents/main/carousel/12.jpg',
-  },
-];
+  'assets/images/contents/main/carousel/1.jpg',
+  'assets/images/contents/main/carousel/2.jpg',
+  'assets/images/contents/main/carousel/3.jpg',
+  'assets/images/contents/main/carousel/4.jpg',
+  'assets/images/contents/main/carousel/5.jpg',
+  'assets/images/contents/main/carousel/6.jpg',
+  'assets/images/contents/main/carousel/7.jpg',
+  'assets/images/contents/main/carousel/8.jpg',
+  'assets/images/contents/main/carousel/9.jpg',
+  'assets/images/contents/main/carousel/10.jpg',
+  'assets/images/contents/main/carousel/11.jpg',
+  'assets/images/contents/main/carousel/12.jpg',
+].map((image) => ({ image }));
 
 export default function WebGlCarousel() {
   return (
     <div className="main-carousel">
       <Canvas>
         <Suspense fallback={null}>
-          <Carousel images={images} />
+          <Carousel images={[...images, ...images]} />
         </Suspense>
       </Canvas>
     </div>
