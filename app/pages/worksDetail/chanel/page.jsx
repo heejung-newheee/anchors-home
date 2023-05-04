9f1c0fb754928c34e6c6dd19c41879bb138780e2
chaewon <cwahn@anchors-biz.com>
2023-05-04 16:09:28 +0900

    [2023-05-04]: chanel 페이지 작업

---
+++ app/pages/worksDetail/chanel/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import WorksTemplate from '@/components/WorksTemplate/WorksTemplate';
+import works from '@/helper/data/json/contents/works/works.json';
+
+export default function Chanel() {
+  return (
+    <>
+      <WorksTemplate
+        visualImgUrl={works.imgUrl + works.content[0].visual}
+        visualChildren={works.content[0].title}
+        disclaimerChildren={works.content[0].description}
+        detailTextDescEn={works.content[0].text.en}
+        detailTextDescKo={works.content[0].text.ko}
+        subVisualImgUrl={works.imgUrl + works.content[0].subVisual}
+        workContent={works.content[0].projectInfomation}
+        firstNextWork={works.content[1]}
+        secondNextWork={works.content[2]}
+        thirdNextWork={works.content[3]}
+      />
+    </>
+  );
+}
