e3c246d9dc650bb5f381618b9c6466b0b3b0543e
“doih” <doiviih@anchors-biz.com>
2023-05-08 17:53:34 +0900

    [2023-05-08] worksDetail 페이지 작업

---
+++ app/pages/worksDetail/wcms/page.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Device from '@/components/Device/Device';
+import ImageArray from '@/components/ImageArray/ImageArray';
+import WorksTemplate from '@/components/WorksTemplate/WorksTemplate';
+import works from '@/helper/data/json/contents/works/works.json';
+
+import './scss/wcms.scss';
+
+export default function WCMS() {
+  return (
+    <>
+      <WorksTemplate
+        custom="Y"
+        visualImgUrl={works.imgUrl + works.content[6].visual}
+        visualChildren={works.content[6].title}
+        disclaimerChildren={works.content[6].description}
+        detailTextDescEn={works.content[6].text.en}
+        detailTextDescKo={works.content[6].text.ko}
+        customChildren={
+          <>
+            <div className="web_img_wrap">
+              <img
+                className="kia_web_img"
+                src="/assets/images/contents/works/img_web_kia.jpg"
+                alt="KIA web 이미지"
+              />
+            </div>
+            <ImageArray
+              data={works.content[6].img_array.img}
+              bgColor={works.content[6].img_array.bg_color}
+              imgUrl={works.imgUrl}
+            />
+            <Device
+              className="wcms_device"
+              type="B"
+              elementTitle="Mobile Web"
+              description="온라인 샵과 홈페이지를 통합함으로서 출시 쇼케이스부터 전시/시승/상담/구매에 이르기까지 고객의 디지털 경험을 강화하였습니다."
+              imgUrl={[
+                '/assets/images/contents/works/img_phone_mobile1_kia.png',
+                '/assets/images/contents/works/img_phone_mobile2_kia.png',
+                '/assets/images/contents/works/img_phone_mobile3_kia.png',
+              ]}
+              caption={['1. Showroom', '2. Exterior', '3. Interior']}
+              alt="WWN app 이미지"
+            />
+          </>
+        }
+        subVisualImgUrl={works.imgUrl + works.content[6].subVisual}
+        workContent={works.content[6].projectInfomation}
+        firstNextWork={works.content[10]}
+        secondNextWork={works.content[11]}
+        thirdNextWork={works.content[12]}
+      />
+    </>
+  );
+}
