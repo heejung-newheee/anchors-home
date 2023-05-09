d657ef2c702e3eb530c6e722c3f7d9cc977b35ff
chaewon <cwahn@anchors-biz.com>
2023-05-09 10:47:14 +0900

    [2023-05-09]: header 초기화

--- helper/view-helper.js
+++ helper/view-helper.js
@@ -3,5 +3,9 @@ export const pagesIsBgBlack = [
   '/pages/worksDetail/myAIA',
   '/pages/worksDetail/chanel',
   '/pages/worksDetail/hyundaiDCXT',
+  '/pages/worksDetail/genesis_Global',
+  '/pages/worksDetail/Jplatform',
+  '/pages/worksDetail/koreanAir_Android',
+  '/pages/worksDetail/koreanAir_KALPOS',
 ];
 export const pagesIsBgWhite = [];
