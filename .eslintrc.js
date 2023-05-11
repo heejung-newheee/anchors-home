c7c820140302650c28159e06c241b4c27f51965c
Hang Young CHO <unclecho@anchors-biz.com>
2023-05-11 14:35:58 +0900

    resolved lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
 module.exports = {
-  extends: ['@anchors'],
+  extends: ['next/core-web-vitals'],
   settings: {},
   rules: {
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      },
-    ],
   },
 };
