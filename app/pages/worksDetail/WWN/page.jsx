e47ec44f94a7a5b8fb56a99436d2c11ef2599b45
chchaa <chchaa@ancrhos-biz.com>
2023-05-15 14:35:21 +0900

    [2023-05-15] works detail json화

--- app/pages/worksDetail/WWN/page.jsx
+++ app/pages/worksDetail/WWN/page.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import DepthTitle from '@/components/DepthTitle/DepthTitle';
 import Device from '@/components/Device/Device';
 import ImageArray from '@/components/ImageArray/ImageArray';
@@ -8,46 +6,44 @@ import seo from '@/helper/data/json/contents/SEO.json';
 import works from '@/helper/data/json/contents/works/works.json';
 
 export const metadata = {
-  title: seo.title.subPage.detail.WWN + ' : ' + seo.title.subPage.works,
+  title: seo.title.subPage.detail.WWN+ ' : ' + seo.title.subPage.works,
 };
 export default function WWN() {
+  
+  const WWN=works.content[11]
+  
   return (
     <>
       <WorksTemplate
         custom="Y"
-        visualImgUrl={works.imgUrl + works.content[11].visual}
-        visualChildren={works.content[11].title}
-        disclaimerChildren={works.content[11].description}
-        detailTextDescEn={works.content[11].text.en}
-        detailTextDescKo={works.content[11].text.ko}
+        visualImgUrl={works.imgUrl + WWN.visual}
+        visualChildren={WWN.title}
+        disclaimerChildren={WWN.description}
+        detailTextDescEn={WWN.text.en}
+        detailTextDescKo={WWN.text.ko}
         customChildren={
           <>
             <DepthTitle depthLevel="1" blindOption="hidden">
-              About WWN Website and App
+              About WWNWebsite and App
             </DepthTitle>
             <ImageArray
-              data={works.content[11].img_array.img}
-              bgColor={works.content[11].img_array.bg_color}
+              data={WWN.custom[0].img_array.img}
+              bgColor={WWN.custom[0].img_array.bg_color}
               imgUrl={works.imgUrl}
             />
             <Device
-              className="wwn_device"
-              type="B"
-              elementTitle="App"
-              description="현대닷컴 Global Template을 신규 구축하고 직관적인 UI를 통해 사용자 편의성을 높였습니다."
-              imgUrl={[
-                '/assets/images/contents/works/img_phone_app1_wwn.png',
-                '/assets/images/contents/works/img_phone_app2_wwn.png',
-                '/assets/images/contents/works/img_phone_app3_wwn.png',
-              ]}
-              caption={['1. Estimation', '2. Hyundai Shop', '3. Find my car']}
-              alt="WWN app 이미지"
+              elementTitle={WWN.custom[1].device.elementTitle}
+              description={WWN.custom[1].device.description}
+              caption={WWN.custom[1].device.caption}
+              imgUrl={WWN.custom[1].device.imgUrl.map( (url) => works.imgUrl + url )}
+              alt={WWN.custom[1].device.alt}
+              type={WWN.custom[1].device.type}
             />
           </>
         }
-        subVisualImgUrl={works.imgUrl + works.content[11].subVisual}
-        workContent={works.content[11].projectInfomation}
-        webSiteUrl={works.content[11].websiteUrl}
+        subVisualImgUrl={works.imgUrl + WWN.subVisual}
+        workContent={WWN.projectInfomation}
+        webSiteUrl={WWN.websiteUrl}
         firstNextWork={works.content[12]}
         secondNextWork={works.content[0]}
         thirdNextWork={works.content[1]}
--- app/pages/worksDetail/jPlatform/page.jsx
+++ app/pages/worksDetail/jPlatform/page.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import DepthTitle from '@/components/DepthTitle/DepthTitle';
 import Description from '@/components/Description/Description';
 import Device from '@/components/Device/Device';
@@ -15,25 +13,28 @@ export const metadata = {
   title: seo.title.subPage.detail.jPlatform + ' : ' + seo.title.subPage.works,
 };
 export default function JPlatform() {
+
+  const JPLATFORM = works.content[2]
+
   return (
     <>
       <WorksTemplate
         custom="Y"
-        visualImgUrl={works.imgUrl + works.content[2].visual}
-        visualChildren={works.content[2].title}
-        disclaimerChildren={works.content[2].description}
-        detailTextDescEn={works.content[2].text.en}
-        detailTextDescKo={works.content[2].text.ko}
-        subVisualImgUrl={works.imgUrl + works.content[2].subVisual}
-        workContent={works.content[2].projectInfomation}
-        webSiteUrl={works.content[2].websiteUrl}
+        visualImgUrl={works.imgUrl + JPLATFORM.visual}
+        visualChildren={JPLATFORM.title}
+        disclaimerChildren={JPLATFORM.description}
+        detailTextDescEn={JPLATFORM.text.en}
+        detailTextDescKo={JPLATFORM.text.ko}
+        subVisualImgUrl={works.imgUrl + JPLATFORM.subVisual}
+        workContent={JPLATFORM.projectInfomation}
+        webSiteUrl={JPLATFORM.websiteUrl}
         customChildren={
           <>
             <section className="video_wrap">
               <DepthTitle depthLevel="1" blindOption="hidden">
                 Promotion Video
               </DepthTitle>
-              <Video videoUrl="/assets/images/video/video_jplatform_main.mp4" />
+              <Video videoUrl={works.videoUrl + JPLATFORM.custom[0].video} />
             </section>
 
             {/* S: main 설명 영역 */}
@@ -42,21 +43,21 @@ export default function JPlatform() {
                 Website Introduction
               </DepthTitle>
               <article className="main_text">
-                <ElementTitle data="Main" />
+                <ElementTitle data={JPLATFORM.custom[1].text} />
                 <Description
-                  data="Customer life cycle 전반의 통합된 고객 경험을 제공하였습니다. <br/> 현대자동차 commerce 영역과 owners 영역을 연계/확장하며 전달력을 높이고, <br/> 입체적인 디자인을 도입하여 고객의 시선과 흥미를 이끌었습니다."
+                  data={JPLATFORM.custom[2].text}
                   innerHTMLOption="Y"
                 />
               </article>
               <article className="main_img">
-                <img src="/assets/images/contents/works/img_web1_jplatform.jpg" alt="현대자동차 J플랫폼 Main 화면 이미지" />
-                <img src="/assets/images/contents/works/img_web2_jplatform.jpg" alt="현대자동차 J플랫폼 Main 화면 이미지" />
+                <img src={works.imgUrl + JPLATFORM.custom[3].image} alt={JPLATFORM.custom[3].alt} />
+                <img src={works.imgUrl + JPLATFORM.custom[4].image} alt={JPLATFORM.custom[4].alt} />
               </article>
             </section>
             {/* E: main 설명 영역 */}
 
             {/* S: full_img 영역 */}
-            <img src="/assets/images/contents/works/img_full_jplatform.jpg" alt="현대자동차 J플랫폼 구축 Ideation 화면 이미지" />
+            <img src={works.imgUrl + JPLATFORM.custom[5].image} alt={JPLATFORM.custom[5].alt} />
             {/* E: full_img 영역 */}
 
             {/* S: PIP 설명 영역 */}
@@ -65,13 +66,13 @@ export default function JPlatform() {
                 PIP Introduction
               </DepthTitle>
               <article className="pip_text">
-                <ElementTitle data="PIP" />
+                <ElementTitle data={JPLATFORM.custom[6].text} />
                 <Description
-                  data="E-Commerce를 통합함으로써 소비까지 이어지는 사용자 경험을 성공적으로 이끌어냈습니다.<br/> 특히, 짜임새 있는 스토리로 하여금 두 모델이 가지고 있는 기능과 효율을 고객들에게 효과적으로 전달하였습니다."
+                  data={JPLATFORM.custom[7].text}
                   innerHTMLOption="Y"
                 />
               </article>
-              <Video videoUrl="/assets/images/video/video_jplatform_3D.mp4" />
+              <Video videoUrl={works.videoUrl + JPLATFORM.custom[8].video} />
             </section>
             {/* E: PIP 설명 영역 */}
 
@@ -85,16 +86,12 @@ export default function JPlatform() {
                   Configurator mobile image
                 </DepthTitle>
                 <Device
-                  elementTitle={['Configurator']}
-                  description={<>3D Configurator 및 쇼룸을 통해 원하는 차량 커스텀 및 간접적 체험이 가능하고, 예상 견적을 제공합니다.</>}
-                  caption={['1. Step setting', '2. Step setting', '3. Total']}
-                  imgUrl={[
-                    '/assets/images/contents/works/img_phone_configurator1_jplatform.png',
-                    '/assets/images/contents/works/img_phone_configurator2_jplatform.png',
-                    '/assets/images/contents/works/img_phone_configurator3_jplatform.png',
-                  ]}
-                  alt={['현대자동차 J플랫폼 Step setting, Total 화면 이미지']}
-                  type="A"
+                  elementTitle={JPLATFORM.custom[9].device.elementTitle}
+                  description={JPLATFORM.custom[9].device.description}
+                  caption={JPLATFORM.custom[9].device.caption}
+                  imgUrl={JPLATFORM.custom[9].device.imgUrl.map( (url) => works.imgUrl + url )}
+                  alt={JPLATFORM.custom[9].device.alt}
+                  type={JPLATFORM.custom[9].device.type}
                 />
               </article>
               <article className="eCommerce_mobile">
@@ -102,16 +99,12 @@ export default function JPlatform() {
                   E-Commerce mobile image
                 </DepthTitle>
                 <Device
-                  elementTitle={['e-Commerce']}
-                  description={<>차량 및 부품 구매가 가능한 온라인 샵을 홈페이지와 통합함으로서, 고객의 구매까지 이어지도록 하였습니다.</>}
-                  caption={['1. Purchase', '2. Shop', '3. Shop category']}
-                  imgUrl={[
-                    '/assets/images/contents/works/img_phone_ecommerce1_jplatform.png',
-                    '/assets/images/contents/works/img_phone_ecommerce2_jplatform.png',
-                    '/assets/images/contents/works/img_phone_ecommerce3_jplatform.png',
-                  ]}
-                  alt={['현대자동차 J플랫폼 Purchase, Shop, Shop category 화면 이미지']}
-                  type="B"
+                  elementTitle={JPLATFORM.custom[10].device.elementTitle}
+                  description={JPLATFORM.custom[10].device.description}
+                  caption={JPLATFORM.custom[10].device.caption}
+                  imgUrl={JPLATFORM.custom[10].device.imgUrl.map( (url) => works.imgUrl + url )}
+                  alt={JPLATFORM.custom[10].device.alt}
+                  type={JPLATFORM.custom[10].device.type}
                 />
               </article>
             </section>
@@ -123,15 +116,15 @@ export default function JPlatform() {
                 Website Identity Introduction
               </DepthTitle>
               <article className="identity_text">
-                <ElementTitle data="Identity system" />
+                <ElementTitle data={JPLATFORM.custom[11].text} />
                 <Description
-                  data="현대의 주컬러를 활용하여 아이덴티티를 명확히 보여주고, 아이콘을 활용한 일관성 있는 UI를 제공합니다."
+                  data={JPLATFORM.custom[12].text}
                   innerHTMLOption="Y"
                 />
               </article>
               <article className="identify_img">
-                <img src="/assets/images/contents/works/img_color_jplatform.jpg" alt="현대자동차 J플랫폼 Identity system 이미지" />
-                <img src="/assets/images/contents/works/img_icon_jplatform.jpg" alt="현대자동차 J플랫폼 Identity system 이미지" />
+                <img src={works.imgUrl + JPLATFORM.custom[13].image} alt={JPLATFORM.custom[13].alt} />
+                <img src={works.imgUrl + JPLATFORM.custom[14].image} alt={JPLATFORM.custom[14].alt} />
               </article>
             </section>
             {/* E: Identity system 설명 영역 */}
