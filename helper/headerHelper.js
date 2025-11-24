6532dd99a9e2b24f032127ee0607e33ba3cdace1
chio.kim <chio.kim@anchors-biz.com>
2025-11-24 16:49:37 +0900

    샤넬 참조 제거

--- helper/headerHelper.js
+++ helper/headerHelper.js
@@ -2,7 +2,7 @@ export const pagesIsBgBlack = [
   '/',
   '/pages/worksDetail/myAIA',
   '/pages/worksDetail/BMS',
-  '/pages/worksDetail/chanel',
+  // '/pages/worksDetail/chanel',
   '/pages/worksDetail/hyundaiDCXT',
   '/pages/worksDetail/genesisGlobal',
   '/pages/worksDetail/jPlatform',
